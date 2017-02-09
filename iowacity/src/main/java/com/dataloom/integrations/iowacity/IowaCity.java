ee4ce0d5b46c05501bc041ff00b765734d776ed3
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-02-09 01:15:08 -0800

    Cleanup integration

--- iowacity/src/main/java/com/dataloom/integrations/iowacity/IowaCity.java
+++ iowacity/src/main/java/com/dataloom/integrations/iowacity/IowaCity.java
@@ -4,8 +4,6 @@ package com.dataloom.integrations.slc;
  * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt;
  */
 
-import com.clearspring.analytics.util.Preconditions;
-import com.dataloom.client.RetrofitFactory.Environment;
 import com.kryptnostic.shuttle.Flight;
 import com.kryptnostic.shuttle.MissionControl;
 import com.kryptnostic.shuttle.Shuttle;
@@ -16,30 +14,26 @@ import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.nio.file.Files;
 import java.util.UUID;
 
 public class IowaCity {
-    private static final Logger            logger            = LoggerFactory.getLogger( IowaCity.class );
-    public static        String            ENTITY_SET_NAME           = "iowajail";
+    private static final Logger logger          = LoggerFactory.getLogger( IowaCity.class );
+    public static        String ENTITY_SET_NAME = "iowajail";
 
     public static void main( String[] args ) throws InterruptedException {
-
-        final String path;
-        final String username;
-        final String password;
+        /*
+         * It's worth noting that we are omitting validation such as making sure enough args were passed in, checking
+         * existence of the file, and making sure authentication was successful. A failure in one of these cases
+         * will cause the program to exit with an exception.
+         */
+        final String path = args[ 0 ];
+        final String username = args[ 1 ];
+        final String password = args[ 2 ];
+        final SparkSession sparkSession = MissionControl.getSparkSession();
+        final String jwtToken = MissionControl.getIdToken( username, password );
+        ;
 
         logger.info( "Using the following idToken: Bearer {}", jwtToken );
-
-        if( args.length==3 ){
-            File f = new File( path );
-            if( f.exists() && !f.isDirectory() ) {
-                path = args[ 0 ];
-            }
-            username = args[1];
-            password = args[2];
-        }
 
         Dataset<Row> payload = sparkSession
                 .read()
@@ -48,14 +42,30 @@ public class IowaCity {
                 .load( path );
 
         Flight flight = Flight.newFlight()
-                .addEntity().to( ENTITY_SET_NAME).as( new FullQualifiedName( "publicsafety.jaildata") )
-                .key(new FullQualifiedName( "general.guid" )   )
-                .addProperty().value( row -> UUID.randomUUID() ).as( new FullQualifiedName( "general.guid" ) ).ok()
-                .addProperty().value( row -> getFirstName(  row.getAs( "Name" ) ) ).as( new FullQualifiedName( "general.firstname" )  ).ok()
-                .addProperty().value( row -> getLastName ( row.getAs( "Name" ) ) ).as( new FullQualifiedName( "general.lastname" )  ).ok()
-                .addProperty().value( row -> row.getAs( "Date of Birth" ) ).as( new FullQualifiedName( "general.dob" )  ).ok()
-                .addProperty().value( row -> row.getAs( "Date Booked" ) ).as( new FullQualifiedName( "publicsafety.datebooked" )  ).ok()
-                .addProperty().value( row -> row.getAs( "Date Released" ) ).as( new FullQualifiedName( "publicsafety.datereleased" )  ).ok()
+                .addEntity().to( ENTITY_SET_NAME ).as( new FullQualifiedName( "publicsafety.jaildata" ) )
+                .key( new FullQualifiedName( "general.guid" ) )
+                .addProperty()
+                .value( row -> UUID.randomUUID() )
+                .as( new FullQualifiedName( "general.guid" ) )
+                .ok()
+                .addProperty()
+                .value( row -> getFirstName( row.getAs( "Name" ) ) )
+                .as( new FullQualifiedName( "general.firstname" ) )
+                .ok()
+                .addProperty().value( row -> getLastName( row.getAs( "Name" ) ) )
+                .as( new FullQualifiedName( "general.lastname" ) )
+                .ok()
+                .addProperty()
+                .value( row -> row.getAs( "Date of Birth" ) )
+                .as( new FullQualifiedName( "general.dob" ) )
+                .ok()
+                .addProperty()
+                .value( row -> row.getAs( "Date Booked" ) )
+                .as( new FullQualifiedName( "publicsafety.datebooked" ) )
+                .ok()
+                .addProperty().value( row -> row.getAs( "Date Released" ) )
+                .as( new FullQualifiedName( "publicsafety.datereleased" ) )
+                .ok()
                 .ok()
                 .done();
 
@@ -63,15 +73,15 @@ public class IowaCity {
         shuttle.launch( flight, payload );
     }
 
-    public static String getFirstName( Object obj  ) {
+    public static String getFirstName( Object obj ) {
         String name = obj.toString();
         String[] names = name.split( "," );
-        return names[1];
+        return names[ 1 ].trim();
     }
 
-    public static String getLastName( Object obj  ) {
+    public static String getLastName( Object obj ) {
         String name = obj.toString();
         String[] names = name.split( "," );
-        return names[0];
+        return names[ 0 ].trim();
     }
 }
