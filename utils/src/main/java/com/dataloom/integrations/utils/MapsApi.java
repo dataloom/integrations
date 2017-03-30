a336589f170c282b9979b6df765b2b6bb3a66cfb
Julia Van Cleve <julia@jmv.me>
2017-03-30 15:54:43 -0700

    Add GeocodedAddress class and Maps API

---
+++ utils/src/main/java/com/dataloom/integrations/utils/MapsApi.java
@@ -0,0 +1,15 @@
+package com.dataloom.integrations.utils;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+import java.util.Map;
+
+/**
+ * Created by julia on 3/2/17.
+ */
+public interface MapsApi {
+
+    @GET("/maps/api/geocode/json")
+    Map<String, Object> gmap(@Query("address") String address, @Query("key") String key );
+}
