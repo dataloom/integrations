048d7dab95bf93f3d95a82cd41bfdbdc0b6b0349
LeylaMova <lmovahedi@gmail.com>
2017-09-28 18:44:54 -0700

    just health demo

--- MockIntegration/src/main/java/com/openlattice/integrations/mockIntegration/demoData.java
+++ MockIntegration/src/main/java/com/openlattice/integrations/mockIntegration/demoData.java
@@ -38,9 +38,8 @@ public class demoData {
 
     public static void main(String[] args) throws InterruptedException {
 
-        final String arrestPath = args[0];
-        final String healthPath = args[1];
-        final String jwtToken = args[2];
+        final String healthPath = args[0];
+        final String jwtToken = args[1];
         //final String username = args[ 1 ];
         //final String password = args[ 2 ];
         final SparkSession sparkSession = MissionControl.getSparkSession();
@@ -50,12 +49,6 @@ public class demoData {
 
         Retrofit retrofit = RetrofitFactory.newClient(environment, () -> jwtToken);
         EdmApi edm = retrofit.create(EdmApi.class);
-
-        Dataset<Row> arrest = sparkSession
-                .read()
-                .format("com.databricks.spark.csv")
-                .option("header", "true")
-                .load(arrestPath);
 
         Dataset<Row> health = sparkSession
                 .read()
@@ -73,109 +66,12 @@ public class demoData {
             reem.ensureEdmElementsExist(requiredEdmElements);
         }
 
-        logger.info("ER Field names: {}", Arrays.asList(arrest.schema().fieldNames()));
         logger.info("ER Field names: {}", Arrays.asList(health.schema().fieldNames()));
-
-        Flight arrestMapping = Flight.newFlight()
-                .createEntities()
-                .addEntity("Suspects")
-                .to("Suspects")
-                .ofType(new FullQualifiedName("general.person"))
-                .key(new FullQualifiedName("nc.SubjectIdentification"))
-                .addProperty("nc.SubjectIdentification")
-                .value(demoData::getSubjectIdentification).ok()
-                .addProperty("nc.PersonGivenName", "FirstName")
-                .addProperty("nc.PersonSurName", "LastName")
-                .addProperty("nc.PersonSex", "Sex")
-                .addProperty("nc.PersonRace", "Race")
-                .addProperty("nc.PersonEthnicity", "Ethnicity")
-                .addProperty("nc.PersonBirthDate", "BirthDate")
-                .endEntity()
-                .addEntity("Incidents")
-                .to("Incidents")
-                .ofType("general.Incident")
-                .key("general.IncidentSequenceID")
-                .addProperty("general.IncidentSequenceID")
-                .value(demoData::getIncidentSequenceID).ok()
-                .addProperty("date.OffenseDate")
-                .value(demoData::safeOffenseDateParse).ok()
-                .addProperty("event.DrugPresentAtArrest", "DrugsPresent")
-                .addProperty("event.WeaponsPresentAtArrest", "WeaponPresent")
-                .addProperty("place.StreetAddress", "IncidentStreet")
-                .addProperty("place.City", "IncidentCity")
-                //.addProperty("event.Disposition", "DIS_Submit")
-                .endEntity()
-                .addEntity("Arrests")
-                .to("Arrests")
-                .ofType("general.Arrest")
-                .key("general.ArrestSequenceID")
-                .addProperty("general.ArrestSequenceID")
-                .value(demoData::getArrestSequenceID).ok()
-                .addProperty("date.ArrestDate")
-                .value(demoData::safeArrestDateParse).ok()
-                .addProperty("person.EyeColorText", "EyeColorText")
-                .addProperty("person.RegisteredSexOffender", "RegisteredSexOffender")
-                .addProperty("event.WarrantType", "WarrantType")
-                .addProperty("event.ArrestCategory", "ArrestCategory")
-                .addProperty("place.ArrestingAgency", "ArrestingAgencyName")
-                .addProperty("place.TransportingAgency", "ArrestingAgencyName")
-                .addProperty("person.ArrestingOfficer", "AO")
-                .addProperty("person.SearchOfficer", "RelOfficer")
-                .addProperty("person.TransportingOfficer", "TranspOfficer")
-                .endEntity()
-                .addEntity("Charges")
-                .to("Charges")
-                .ofType("general.Charge")
-                .key("general.ChargeSequenceID")
-                .addProperty("general.ChargeSequenceID")
-                .value(demoData::getChargeSequenceID).ok()
-                .addProperty("event.OffenseStateCodeSection", "ChargeLevelState")
-                .addProperty("event.OffenseLocalCodeSection", "OffenseLocalStatute")
-                .addProperty("event.OffenseDescription", "OffenseLocalText")
-                .addProperty("event.OffenseSeverity", "ChargeLevel")
-                .addProperty("date.ChargeReleaseDate")
-                .value(demoData::safeReleaseDateParse).ok()
-                .endEntity()
-                .endEntities()
-                .createAssociations()
-                .addAssociation("ArrestedIn")
-                .ofType("general.ArrestedIn").to("ArrestedIn")
-                .fromEntity("Suspects")
-                .toEntity("Arrests")
-                .key("nc.SubjectIdentification", "general.ArrestSequenceID")
-                .addProperty("nc.SubjectIdentification")
-                .value(demoData::getSubjectIdentification).ok()
-                .addProperty("general.ArrestSequenceID")
-                .value(demoData::getArrestSequenceID).ok()
-                .endAssociation()
-                .addAssociation("AppearsIn")
-                .ofType("general.AppearsIn").to("AppearsIn")
-                .fromEntity("Suspects")
-                .toEntity("Incidents")
-                .key("nc.SubjectIdentification", "general.IncidentSequenceID")
-                .addProperty("nc.SubjectIdentification")
-                .value(demoData::getSubjectIdentification)
-                .ok()
-                .addProperty("general.IncidentSequenceID")
-                .value(demoData::getIncidentSequenceID).ok()
-                .endAssociation()
-                .addAssociation("ChargedWith")
-                .ofType("general.ChargedWith").to("ChargedWith")
-                .fromEntity("Suspects")
-                .toEntity("Charges")
-                .key("nc.SubjectIdentification", "general.ChargeSequenceID")
-                .addProperty("nc.SubjectIdentification")
-                .value(demoData::getSubjectIdentification).ok()
-                .addProperty("general.ChargeSequenceID")
-                .value(demoData::getChargeSequenceID).ok()
-                .endAssociation()
-                .endAssociations()
-                .done();
 
     Flight healthMapping = Flight.newFlight()
             .createEntities()
             .addEntity("Patients")
-            .to("Patients")
+            .to("DemoPatients")
             .ofType(new FullQualifiedName("general.person"))
             .key(new FullQualifiedName("nc.SubjectIdentification"))
             .addProperty("nc.SubjectIdentification")
@@ -185,10 +81,12 @@ public class demoData {
             .addProperty("nc.PersonSex", "Sex")
             .addProperty("nc.PersonRace", "Race")
             .addProperty("nc.PersonEthnicity", "Ethnicity")
-            .addProperty("nc.PersonBirthDate", "BirthDate")
+            .addProperty("nc.PersonBirthDate")
+            .value(demoData::safeDOBParse).ok()
+            .addProperty("nc.PersonEyeColorText", "EyeColorText")
             .endEntity()
             .addEntity("Providers")
-            .to("Providers")
+            .to("DemoProviders")
             .ofType("general.person")
             .key("nc.SubjectIdentification")
             .addProperty("nc.SubjectIdentification")
@@ -197,21 +95,24 @@ public class demoData {
             .addProperty("nc.PersonSurName", "providerLNames")
             .endEntity()
             .addEntity("Appointments")
-            .to("Appointments")
+            .to("DemoAppointments")
             .ofType("general.Appointment")
-            .key("general.StringID")
-            .addProperty("general.StringID", "id")
-            .addProperty("date.UpcomingVisit", "upcomingAppt")
-            .addProperty("date.MissedAppointment", "missedAppt")
-            .addProperty("date.MissedAppointment", "missedApptFirst")
+            .key("general.AppointmentSequenceID")
+            .addProperty("general.AppointmentSequenceID")
+            .value(demoData::getAppointmentSequenceID).ok()
+            .addProperty("date.UpcomingVisit")
+            .value(demoData::safeupcomingApptParse).ok()
+            .addProperty("date.MissedAppointment")
+            .value(demoData::safemissedApptParse).ok()
             .addProperty("event.MissedAppointmentCount", "NumMissedAppt")
             .addProperty("event.AppointmentType", "ApptTypes")
             .endEntity()
             .addEntity("AdmissionRecords")
-            .to("AdmissionRecords")
+            .to("DemoAdmissionRecords")
             .ofType("general.AdmissionRecord")
-            .key("general.StringID")
-            .addProperty("general.StringID", "id")
+            .key("general.AdmissionSequenceID")
+            .addProperty("general.AdmissionSequenceID")
+            .value(demoData::getAdmissionSequenceID).ok()
             .addProperty("date.AdmissionDate")
             .value(demoData::safeAdmissionDateParse).ok()
             .addProperty("date.LastUsed")
@@ -228,7 +129,7 @@ public class demoData {
             .addProperty("person.AgeAtEvent", "Age")
             .endEntity()
             .addEntity("MedicalAssessments")
-            .to("MedicalAssessments")
+            .to("DemoMedicalAssessments")
             .ofType("general.MedicalAssessment")
             .key("general.AssessmentSequenceID")
             .addProperty("general.AssessmentSequenceID")
@@ -244,15 +145,29 @@ public class demoData {
             .endEntity()
             .endEntities()
             .createAssociations()
-            .addAssociation("AppearsIn")
-            .ofType("general.AppearsIn").to("AppearsIn")
+            .addAssociation("AppearsInAppointment")
+            .ofType("general.AppearsIn").to("HealthAppearsIn")
             .fromEntity("Patients")
             .toEntity("Appointments")
-            .key("nc.SubjectIdentification", "general.StringID")
+            .key("nc.SubjectIdentification", "general.AppointmentSequenceID")
             .addProperty("nc.SubjectIdentification")
             .value(demoData::getSubjectIdentification)
             .ok()
-            .addProperty("general.StringID", "id")
+            .addProperty("general.AppointmentSequenceID")
+            .value(demoData::getAppointmentSequenceID)
+            .ok()
+            .endAssociation()
+            .addAssociation("AppearsInAdmission")
+            .ofType("general.AppearsIn").to("HealthAppearsIn")
+            .fromEntity("Patients")
+            .toEntity("AdmissionRecords")
+            .key("nc.SubjectIdentification", "general.AdmissionSequenceID")
+            .addProperty("nc.SubjectIdentification")
+            .value(demoData::getSubjectIdentification)
+            .ok()
+            .addProperty("general.AdmissionSequenceID")
+            .value(demoData::getAdmissionSequenceID)
+            .ok()
             .endAssociation()
             .addAssociation("VisitedIn")
             .ofType("general.VisitedIn").to("VisitedIn")
@@ -265,12 +180,35 @@ public class demoData {
             .addProperty("general.AssessmentSequenceID")
             .value(demoData::getAssessmentSequenceID).ok()
             .endAssociation()
+            .addAssociation("AppearsInAssessment")
+            .ofType("general.AppearsIn").to("HealthAppearsIn")
+            .fromEntity("Providers")
+            .toEntity("AdmissionRecords")
+            .key("nc.SubjectIdentification", "general.AdmissionSequenceID")
+            .addProperty("nc.SubjectIdentification")
+            .value(demoData::getSubjectIdentification)
+            .ok()
+            .addProperty("general.AdmissionSequenceID")
+            .value(demoData::getAdmissionSequenceID)
+            .ok()
+            .endAssociation()
+            .addAssociation("AppearsInVisit")
+            .ofType("general.AppearsIn").to("HealthAppearsIn")
+            .fromEntity("Providers")
+            .toEntity("MedicalAssessments")
+            .key("nc.SubjectIdentification", "general.AssessmentSequenceID")
+            .addProperty("nc.SubjectIdentification")
+            .value(demoData::getSubjectIdentification)
+            .ok()
+            .addProperty("general.AssessmentSequenceID")
+            .value(demoData::getAssessmentSequenceID)
+            .ok()
+            .endAssociation()
             .endAssociations()
             .done();
 
         Shuttle shuttle = new Shuttle(environment, jwtToken);
         Map<Flight, Dataset<Row>> flights = new HashMap<>(2);
-        flights.put(arrestMapping, arrest);
         flights.put(healthMapping, health);
 
         shuttle.launch(flights);
@@ -279,47 +217,6 @@ public class demoData {
     public static String getSubjectIdentification(Row row) {
 
         return "PERSON-" + row.getAs("SubjectIdentification");
-    }
-
-    public static String getArrestSequenceID(Row row) {
-
-        return "ARREST-" + row.getAs("X");
-    }
-
-    public static String getIncidentSequenceID(Row row) {
-
-        return "INCIDENT-" + row.getAs("X");
-    }
-
-    public static String getChargeSequenceID(Row row) {
-
-        return "CHARGE-" + row.getAs("X");
-    }
-
-    public static String safeDOBParse(Row row) {
-        String dob = row.getAs("BirthDate");
-        return bdHelper.parse(dob);
-    }
-
-    public static String safeArrestDateParse(Row row) {
-        String arrestDate = row.getAs("ArrestDate");
-        return dtHelper.parse(arrestDate);
-    }
-
-
-    public static String safeOffenseDateParse(Row row) {
-        String OffDate = row.getAs("IncidentDate");
-        return dtHelper.parse(OffDate);
-    }
-
-    public static String safeERDParse(Row row) {
-        String expReleaseDate = row.getAs("Exp_Release_Date");
-        return dtHelper.parse(expReleaseDate);
-    }
-
-    public static String safeReleaseDateParse(Row row) {
-        String releaseDate = row.getAs("ReleaseDate");
-        return dtHelper.parse(releaseDate);
     }
 
     public static String safeLastUsedParse(Row row) {
@@ -337,4 +234,28 @@ public class demoData {
         return "ASSESSMENT-" + row.getAs("id");
     }
 
+    public static String getAdmissionSequenceID(Row row) {
+
+        return "ADMISSION-" + row.getAs("id");
+    }
+
+    public static String getAppointmentSequenceID(Row row) {
+
+        return "APPOINTMENT-" + row.getAs("id");
+    }
+
+    public static String safeDOBParse(Row row) {
+        String dob = row.getAs("BirthDate");
+        return bdHelper.parse(dob);
+    }
+
+    public static String safeupcomingApptParse(Row row) {
+        String upcomingAppt = row.getAs("upcomingAppt");
+        return dtHelper.parse(upcomingAppt);
+    }
+
+    public static String safemissedApptParse(Row row) {
+        String missedAppt = row.getAs("missedAppt");
+        return dtHelper.parse(missedAppt);
+    }
 }
